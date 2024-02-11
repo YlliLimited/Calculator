6e5772657401ded54b92946eb9fca0c7080c993c
Yll Kelmendi <yllkelmendi05@gmail.com>
2024-02-11 16:03:36 +0100

    modified:   app.js 	modified:   style.css

--- app.js
+++ app.js
@@ -1,41 +1,76 @@
 // Calculator
+
+
 let result = '';
 let resultDiv = document.querySelector(".calcResult");
 
 
+function calculatorFontSize () {
+  if (screen.width < 500) {
+    if (result.length > 19){
+      resultDiv.style.fontSize = '5cqw';
+    }
+    else if (result.length > 12) {
+      resultDiv.style.fontSize = '7cqw';
+    }
+    else {
+      resultDiv.style.fontSize = '11cqw';
+    }
+  }
+  else if (screen.width < 800) {
+    if (result.length > 19){
+      resultDiv.style.fontSize = '20px';
+    }
+    else if (result.length > 12) {
+      resultDiv.style.fontSize = '30px';
+    }
+    else {
+      resultDiv.style.fontSize = '50px';
+    }
+  } else {
+    if (result.length > 19){
+      resultDiv.style.fontSize = '30px';
+    }
+    else if (result.length > 12) {
+      resultDiv.style.fontSize = '40px';
+    }
+    else {
+      resultDiv.style.fontSize = '60px';
+    }
+  }
+}
+
 function evaluateResult() {
   let evaluation = eval(result);
 
-  if (evaluation.toString().length > 12) {
-    resultDiv.style.fontSize = '40px';
+  if ( screen.width < 800){
+    if (evaluation.toString().length > 12) {
+      resultDiv.style.fontSize = '30px';
+    }
+    else {
+      resultDiv.style.fontSize = '50px';
+    }
   }
-  else {
-    resultDiv.style.fontSize = '60px';
+  else{
+    if (evaluation.toString().length > 12) {
+      resultDiv.style.fontSize = '40px';
+    }
+    else {
+      resultDiv.style.fontSize = '60px';
+    }
   }
 
   resultDiv.innerHTML = evaluation
 };
-
 function C () {
   result = ''
 
   resultDiv.innerHTML = result;
 }
-
 function addNumber(number) {
   result += number;
 
-  if (result.length > 19){
-    resultDiv.style.fontSize = '30px';
-  }
-  else if (result.length > 12) {
-    resultDiv.style.fontSize = '40px';
-  }
-  else {
-    resultDiv.style.fontSize = '60px';
-  }
-
-  console.log(result.length);
+  calculatorFontSize ();
 
   resultDiv.innerHTML = result;
 }
@@ -43,105 +78,49 @@ function addNumber(number) {
 function addition(){
   result += ' + ';
 
-  if (result.length > 19){
-    resultDiv.style.fontSize = '30px';
-  }
-  else if (result.length > 12) {
-    resultDiv.style.fontSize = '40px';
-  }
-  else {
-    resultDiv.style.fontSize = '60px';
-  }
+  calculatorFontSize ();
   
   resultDiv.innerHTML = result;
 }
 function substraction(){
   result += ' - ';
 
-  if (result.length > 19){
-    resultDiv.style.fontSize = '30px';
-  }
-  else if (result.length > 12) {
-    resultDiv.style.fontSize = '40px';
-  }
-  else {
-    resultDiv.style.fontSize = '60px';
-  }
+  calculatorFontSize ();
 
   resultDiv.innerHTML = result;
 }
 function multiplication(){
   result += ' * ';
 
-  if (result.length > 19){
-    resultDiv.style.fontSize = '30px';
-  }
-  else if (result.length > 12) {
-    resultDiv.style.fontSize = '40px';
-  }
-  else {
-    resultDiv.style.fontSize = '60px';
-  }
+  calculatorFontSize ();
 
   resultDiv.innerHTML = result;
 }
 function division(){
   result += ' / ';
 
-  if (result.length > 19){
-    resultDiv.style.fontSize = '30px';
-  }
-  else if (result.length > 12) {
-    resultDiv.style.fontSize = '40px';
-  }
-  else {
-    resultDiv.style.fontSize = '60px';
-  }
+  calculatorFontSize ();
 
   resultDiv.innerHTML = result;
 }
 function decimal(){
   result += '.';
 
-  if (result.length > 19){
-    resultDiv.style.fontSize = '30px';
-  }
-  else if (result.length > 12) {
-    resultDiv.style.fontSize = '40px';
-  }
-  else {
-    resultDiv.style.fontSize = '60px';
-  }
+  calculatorFontSize ();
 
   resultDiv.innerHTML = result;
 }
 function percent () {
    result = result / 100;
 
-   if (result.length > 19){
-    resultDiv.style.fontSize = '30px';
-  }
-  else if (result.length > 12) {
-    resultDiv.style.fontSize = '40px';
-  }
-  else {
-    resultDiv.style.fontSize = '60px';
-  }
+   calculatorFontSize ();
 
    resultDiv.innerHTML = result;
 }
  function negNumber () {
    result += '-';
 
-   if (result.length > 19){
-    resultDiv.style.fontSize = '30px';
-  }
-  else if (result.length > 12) {
-    resultDiv.style.fontSize = '40px';
-  }
-  else {
-    resultDiv.style.fontSize = '60px';
-  }resultDiv.style.fontSize = '60px';
+   calculatorFontSize ();
 
    resultDiv.innerHTML = result;
 }
